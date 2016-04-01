eead0f1631f3c68905f13dd53cf097bda8528cd3
martinbaciga <bacigalupo.martin@gmail.com>
2016-04-01 17:40:03 -0300

    fixes

--- app/src/main/java/co/lateralview/androidskeleton/infrastructure/communication/rest/request/ExampleRequest.java
+++
@@ -1,5 +0,0 @@
-package co.lateralview.androidskeleton.infrastructure.communication.rest.request;
-
-public class ExampleRequest
-{
-}
--- app/src/main/java/co/lateralview/androidskeleton/infrastructure/communication/rest/responseDTO/ExampleDTO.java
+++
@@ -1,5 +0,0 @@
-package co.lateralview.androidskeleton.infrastructure.communication.rest.responseDTO;
-
-public class ExampleDTO
-{
-}
